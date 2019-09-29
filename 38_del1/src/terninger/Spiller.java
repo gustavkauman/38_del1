58badcf6047d38f1d48b459564aac58c8d823052
David Mikkelsen <Davidmikkelsen@live.dk>
2019-09-29 13:38:00 +0200

    Oprydning 1

--- 38_del1/src/terninger/Spiller.java
+++
@@ -1,9 +0,0 @@
-package terninger;
-
-public class Spiller {
-
-    //Redigering i spiller klasse
-
-    system.out.println("2");
-
-}
--- 38_del1/src/terninger/Terninger.java
+++
@@ -1,7 +0,0 @@
-package terninger;
-
-public class Terninger {
-
-
-
-}
