f96950b8a1f3c9ef20f8e235251ba4f23c91b1f2
Sebastian_Bjerre <sebastianbjerre@hotmail.com>
2019-10-04 09:19:44 +0200

    Ny dev branch

---
+++ src/terningespil/DiceCup.java
@@ -0,0 +1,63 @@
+package terningespil;
+
+public class DiceCup {
+
+    // Variable
+    private final Dice t1;
+    private final Dice t2;
+    private int outcome1, outcome2, sum;
+
+    // Kontruktør. Laver to terninger.
+    public DiceCup() {
+        t1 = new Dice();
+        t2 = new Dice();
+    }
+
+    // Metoder. Slår de to terninger, med metoden "roll" fra "Terning"-klassen.
+    public void throwDice() {
+        outcome1 = t1.roll();
+        outcome2 = t2.roll();
+    }
+
+    //lægger de to facevalues sammen og gemmer dem som sum
+    public int sumFace() {
+        sum = outcome1 + outcome2;
+        return sum;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    // Returnere en tekst med værdien af terningernes rul.
+    public String toStringOutcome() {
+        return "Terning 1: " + outcome1 + "\n" + "Terning 2: " + outcome2 + "\n";
+    }
+
+    // Tjekker om der er slået to ens værdier. Returnere boolean.
+    public boolean checkIfEqual() {
+        return (outcome1 == outcome2);
+    }
+
+    //Tjekker om terningerne har slået to ettere. Returnere boolean.
+    public boolean checkIfDoubleOne() {
+        return (outcome1 == 1 && outcome2 == 1);
+    }
+
+    // Tjekker om terningerne har slået to seksere
+    public boolean checkIfDoubleSix() {
+
+        return (outcome1 == 6 && outcome2 == 6);
+    }
+
+    // Sætter værdien af udfaldende til den ønskede værdi
+    public void setUdfald(final int dice1, final int dice2) {
+
+        outcome1 = dice1;
+        outcome2 = dice2;
+
+    }
+
+}
+
+
--- src/terningespil/Player.java
+++ src/terningespil/Player.java
@@ -1,18 +1,18 @@
 package terningespil;
 
-public class Spiller{
+public class Player {
 
     //instance var. (attributter)
 
     private int point;
 
     //Konstruktør
-    public Spiller(){
+    public Player() {
     }
 
 
     //tager sum fra raflebæger og ligger til samlede point
-    public void sumPoint(int sum){
+    public void sumPoint(final int sum) {
         point += sum;
     }
 
@@ -21,31 +21,31 @@ public class Spiller{
     }
 
     //metode til at starte spiller fra 0 hvis spiller slår to ettere
-    public void startForfra(boolean input){
-        if(input)
+    public void startOver(final boolean input) {
+        if (input)
             point = 0;
     }
 
     // retunerer en string, der angiver spillerens nuværende score (point)
-    public String toString(){
+    public String toStringScore() {
 
         return "Score: " + point;
     }
 
     // retunerer en string, der ønsker tillykke med sejren. spilleren der har vundet skal angives som argument i metoden
-    public String toStringWinner(String spiller){
+    public String toStringWinner(final String player) {
 
-        return "Tillykke "+ spiller +". Du har vundet!";
+        return "Tillykke " + player + ". Du har vundet!";
     }
 
     // retunerer en string, der informerer spilleren der har slået to ettere om, at alle point er mistet.
-    public String toStringTooBad(){
+    public String toStringTooBad() {
 
         return "Desværre!. Du slog to ettere og mister alle dine point";
     }
 
     // retunrer en string, der informerer spilleren om at han/hun/hen, har slået to ens og dermed får et ekstra slag
-    public String toStringExtra(){
+    public String toStringExtra() {
 
         return "Du slog to seksere!. Du får et ekstra slag\n";
     }
